44780d88e796340a40f202d3924b1cb8716eee89
shiva sharma <shva.sharma29@gmail.com>
2023-01-24 10:20:43 +0530

    fix date and like button dynamic

--- src/card.js
+++ src/card.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './card.css'
 const Card=({post})=>{
-    const fullDate = new Date();
-    const d = fullDate.getDate();
-    const m = fullDate.getMonth();
-    const y = fullDate.getFullYear();
-    const date = `${d}-${m+1}-${y}`
+    // const fullDate = new Date();
+    // const d = fullDate.getDate();
+    // const m = fullDate.getMonth();
+    // const y = fullDate.getFullYear();
+    // const date = `${d}-${m+1}-${y}`;
+
+    const db_date = (post.createdAt).split("-");
+    const day = db_date[2];
+    const date = `${day[0]}${day[1]}-${db_date[1]}-${db_date[0]}`
+    
+    const [like,setLike] = useState(25);
+    const likebtn=()=>{
+        setLike(like+1);
+    }
     return(
         <>
             <section className="card">
@@ -21,14 +31,14 @@ const Card=({post})=>{
                     <img src={post.postImage} alt="place"/>
                 </section>
                 <section className="card-actions">
-                    <span><i className="fa fa-heart" aria-hidden="true"></i></span>
+                    <span onClick={likebtn}><i className="fa fa-heart" aria-hidden="true"></i></span>
                     <span><i className="fa fa-paper-plane" aria-hidden="true"></i></span>
                     {/* <span>{post.date}</span> */}
                     <span>{date}</span>
                 </section>
                 <section className="likes">
                     {/* {post.likes} Likes */}
-                    likes 25
+                    likes {like}
                 </section>
                 <section className="description">
                     {post.description} 
